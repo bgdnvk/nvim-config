a9e1c72611bf68a5fc96d56c6a064119f3f8ff93
Bogdan Novykov Chumak <bnovykov@gmail.com>
2024-02-16 15:43:43 +0100

    lsp fix

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Setup language servers.
+-- local lspconfig = require("lspconfig")
+-- lspconfig.pyright.setup({})
+-- lspconfig.tsserver.setup({})
+-- lspconfig.rust_analyzer.setup({
+--   -- Server-specific settings. See `:help lspconfig-setup`
+--   settings = {
+--     ["rust-analyzer"] = {},
+--   },
+-- })
